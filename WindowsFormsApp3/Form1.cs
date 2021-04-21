8a82addcbf42d0551cbb9b1e35c6b63d7c7eea9e
Баринов Марсель Алексеевич <211902@mck-ktits.ru>
2021-04-21 14:24:37 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsApp3/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            int valX; int valY; int valW; int valH;            
+            InitializeComponent();
+        }
+        Point click;
+        Graphics g;
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            g = CreateGraphics();
+            g.Clear(Color.Black);
+            g.DrawRectangle(Pens.Black, 100, 100, 300, 200);
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+
+
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
